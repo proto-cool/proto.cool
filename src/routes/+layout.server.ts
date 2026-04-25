d5b558321920f684375ae7acd758e9e125ce5b8b
protocol7 <me@proto.cool>
2026-04-25 09:25:18 -0600

    shell: load chrome data alongside theme/mode

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,8 @@
-// src/routes/+layout.server.ts
 import type { LayoutServerLoad } from './$types';
+import { resolveChromeData } from '$lib/shell/chrome';
 
 export const load: LayoutServerLoad = ({ locals }) => ({
 	theme: locals.theme,
-	mode: locals.mode
+	mode: locals.mode,
+	chrome: resolveChromeData()
 });
